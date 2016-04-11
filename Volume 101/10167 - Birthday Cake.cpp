fb25d1ee785f842a494365be42e6ac97c69f1a48
noname <noname19930930@gmail.com>
2016-04-12 00:50:44 +0800

    Fixed Bug

--- Volume 101/10167 - Birthday Cake.cpp
+++ Volume 101/10167 - Birthday Cake.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<vector>
-#include<fstream>
 
 using namespace std;
 
@@ -8,54 +7,55 @@ class point
 {
 	public:
 		point(int a, int b)
-		:x(a), y(b) {}
+			:x(a), y(b) {}
 		point() {}
 		int x, y;
 };
 
 int main()
 {
-	ifstream infile;
-	infile.open("input.txt", ios::in);
-
-	ofstream outfile;
-	outfile.open("output.txt", ios::out);
-
 	int n;
-	while (infile >> n && n)
+	while (cin >> n && n)
 	{
 		//cherry
 		vector<point> p;
 		for (int i = 0; i < 2 * n; i++)
 		{
 			int x, y;
-			infile >> x >> y;
+			cin >> x >> y;
 			p.push_back(point(x, y));
 		}
 
 		bool ok = true;
 		for (int i = -500; i < 501 && ok; i++)
+		for (int j = -500; j < 501 && ok; j++)
 		{
-			for (int j = -500; j < 501 && ok; j++)
+			int upper = 0;
+			int lower = 0;
+
+			for (int k = 0; k < p.size(); k++)
 			{
-				int upper = 0;
-				int lower = 0;
+				int tmp = p[k].x * i + p[k].y * j;
+				if (tmp > 0)
+					upper++;
+				else if (tmp < 0)
+					lower++;
+				else
+					break;
 
-				for (int k = 0; k < p.size(); k++)
+				//wrong
+				if (upper > n || lower > n)
 				{
-					int tmp = p[k].x * i + p[k].y * j;
-					if (tmp > 0)
-						upper++;
-					else if (tmp < 0)
-						lower++;
-
-					if (upper == lower && upper)
-					{
-						outfile << i << " " << j << endl;
-						printf("%d %d\n", i, j);
-						ok = false;
-						break;
-					}
+					upper = 0;
+					lower = 0;
+					break;
+				}
+				//answer
+				if (upper == n && lower == n)
+				{
+					cout << i << " " << j << endl;
+					ok = false;
+					break;
 				}
 			}
 		}
