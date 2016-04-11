fda025f9bac2e0081dde083bfcb739877246b2d1
noname <noname19930930@gmail.com>
2016-04-12 00:50:50 +0800

    AC

---
+++ Volume 104/10499 - The Land of Justice.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long n;
+    while(cin >> n && n>0)
+    {
+        if(n==1)
+            cout << "0%" << endl;
+        else
+            cout << n*25 << "%" << endl;
+    }
+    return 0;
+}
